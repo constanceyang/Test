f60c4e611d3b204ed7871632cb0086a4bb58323c
Constance Yang <conyan@oitadmins-macbook-pro.local>
2016-07-25 15:09:48 +0100

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Constance Yang on 25/07/2016.
+//  Copyright © 2016 Constance Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Constance Yang on 25/07/2016.
+//  Copyright © 2016 Constance Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
