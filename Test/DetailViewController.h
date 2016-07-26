73e6576c2943e06bd04a66eae0504471fcc6fe83
Constance Yang <conyan@OITAdmins-MacBook-Pro.local>
2016-07-26 13:56:01 +0100

    add detail view controller

---
+++ Test/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Test
+//
+//  Created by Constance Yang on 26/07/2016.
+//  Copyright © 2016 Constance Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
